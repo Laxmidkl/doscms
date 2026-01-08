2586a7847bbf46de2e86793a7cb592cec5358ae7
Laxmidkl <dhakalaxmi74@gmail.com>
2026-01-08 15:39:20 +0545

    Add sidebar component with navigation items

--- src/component/Layout/Sidebar.jsx
+++ src/component/Layout/Sidebar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { FaRegFileAlt } from "react-icons/fa";
+import { GoHome } from "react-icons/go";
+import { MdOutlineEventNote } from "react-icons/md";
+import { RiBloggerLine } from "react-icons/ri";
 
 const Sidebar = () => {
-  return (
-    <div className=' w-64 bg-red-300'>Sidebar</div>
-  )
-}
+  const link = [
+    {
+      name: "Home",
+      icon: <GoHome />,
+    },
 
-export default Sidebar
+    {
+      name: "About",
+      icon: <FaRegFileAlt />,
+    },
+
+    {
+      name: "Blog",
+      icon: <RiBloggerLine />,
+    },
+    {
+      name: "Events",
+      icon: <MdOutlineEventNote />,
+    },
+  ];
+  return (
+    <aside className="w-72 bg-gray-300 border border-gray-400 p-2 font-semibold  flex flex-col">
+      {link.map((links, index) => (
+        <div
+          key={index}
+          className="text-black rounded-lg p-3 mt-5 hover:bg-red-500 hover:text-white flex gap-2 text-lg"
+        >
+          <span className=" mt-1.5 ml-4 ">
+          {links.icon}
+          </span>
+          {links.name} 
+        </div>
+      ))}
+    </aside>
+  );
+};
+
+export default Sidebar;
