c8ccf0975ec648d76a4f8a3f83bc456e2d3e6384
Laxmidkl <dhakalaxmi74@gmail.com>
2026-01-08 16:20:05 +0545

    Enable sidebar navigation with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainLayout from '../src/component/Layout/MainLayout'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "../src/component/Layout/MainLayout";
+import Banner from "../src/component/banner/HomeBanner";
+import Home from "./component/pages/Home";
+import Blog from "../src/component/pages/Blog"
+import About from "./component/pages/About";
+import Events from "./component/pages/Events";
+
 
 const App = () => {
   return (
-  
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element ={<MainLayout/>}>
-    
-    <Route/>
-
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+        <Route path="" element ={<Home/>}/>
+        <Route path="/blog" element ={<Blog/>}/>
+        <Route path="/about" element ={<About/>}/>
+        <Route path="/event" element ={<Events/>}/>
+        <Route path="/banner" element ={<Banner/>}/>
+          <Route />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/Layout/Sidebar.jsx
+++ src/component/Layout/Sidebar.jsx
@@ -2,39 +2,49 @@ import { FaRegFileAlt } from "react-icons/fa";
 import { GoHome } from "react-icons/go";
 import { MdOutlineEventNote } from "react-icons/md";
 import { RiBloggerLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const link = [
+  const item = [
     {
       name: "Home",
       icon: <GoHome />,
+      link :""
     },
 
     {
       name: "About",
       icon: <FaRegFileAlt />,
+      link :"/about"
     },
 
     {
       name: "Blog",
       icon: <RiBloggerLine />,
+      link :"/blog"
     },
     {
       name: "Events",
       icon: <MdOutlineEventNote />,
+      link :"/event"
+    },
+    {
+      name: "Banner",
+      icon: <FaRegFileAlt />,
+      link :"/banner"
     },
   ];
+  const navigate = useNavigate()
   return (
     <aside className="w-72 bg-gray-300 border border-gray-400 p-2 font-semibold  flex flex-col">
-      {link.map((links, index) => (
+      {item.map((nav, index) => (
         <div
           key={index}
-          className="text-black rounded-lg p-3 mt-5 hover:bg-red-500 hover:text-white flex gap-2 text-lg"
+          onClick={() => navigate(nav.link)}
+          className="text-black rounded-lg p-3 mt-5 hover:bg-red-500 hover:text-white flex gap-2 text-lg cursor-pointer"
         >
-          <span className=" mt-1.5 ml-4 ">
-          {links.icon}
-          </span>
-          {links.name} 
+          <span className=" mt-1.5 ml-4 ">{nav.icon}</span>
+          {nav.name} 
         </div>
       ))}
     </aside>
